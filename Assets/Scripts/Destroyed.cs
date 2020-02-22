ad7d572bc1152aee44c577510ea8ab672e37c879
Husni <mohhusnimubaraq@gmail.com>
2020-02-22 23:34:43 +0700

    update

---
+++ Assets/Scripts/Destroyed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyed : MonoBehaviour
+{
+    public bool isPlayer = true;
+    private void OnDestroy()
+    {
+        if (!DataGame.isGameOver)
+            if (isPlayer)
+            {
+                DataGame.isGameOver = true;
+                DataGame.isComplete = false;
+                Debug.Log("Player Lost");
+            }
+            else
+            {
+                DataGame.isGameOver = true;
+                DataGame.isComplete = true;
+                Debug.Log("Player Win");
+            }
+    }
+}
